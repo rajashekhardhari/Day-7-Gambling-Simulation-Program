e9486633d0b84518ddaed1d7338618d062a2d75b
srajashekhar <udayarajashekhar@gmail.com>
2022-07-17 08:59:35 +0530

    Added: README.md and .gitignore file

---
+++ src/com/bridgelabz/gambling/GamblingSimulationProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.gambling;
+
+public class GamblingSimulationProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Gambling Simulation Program");
+	}
+
+}
