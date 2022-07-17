4b5eea83413931c6878da6116c7c2f8cec338c9f
srajashekhar <udayarajashekhar@gmail.com>
2022-07-17 10:48:29 +0530

    Added: win or loose program using while loop

--- src/com/bridgelabz/gambling/GamblingSimulationProgram.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProgram.java
@@ -4,25 +4,24 @@ import java.util.Random;
 
 public class GamblingSimulationProgram {
 
-	public static final int initial_Stake = 100;
-	public static final int stake_Bet = 1;
+	public static int initial_Stake = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Simulation Program");
 
-		int totalStake = 0;
-
+		int playCount = 0;
 		Random random = new Random();
-		int game = random.nextInt(2);
-		if (game == 1) {
-			totalStake = totalStake + stake_Bet;
 
-			System.out.println("Gambler won the bet and have total stake:" + totalStake);
-		} else {
-			totalStake = totalStake - stake_Bet;
-
-			System.out.println("Gambler lost the bet and have total stake:" + totalStake);
+		while (initial_Stake != 0 && initial_Stake != 200) {
+			playCount++;
+			System.out.println("initial money " + initial_Stake);
+			int bet = random.nextInt(2);
+			if (bet == 1) {
+				initial_Stake++;
+			} else {
+				initial_Stake--;
+			}
+			System.out.println("Remaining money " + initial_Stake);
 		}
-
 	}
 }
